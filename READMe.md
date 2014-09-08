138986389a2c3f391d244fd66c5b0e171767681a
Andreas Møller <andreas.moller@gmail.com>
2014-09-08 11:13:15 +0100

    updates

---
+++ READMe.md
@@ -0,0 +1,3 @@
+#Sentia Hoarder
+[ ![Codeship Status for Sentiaio/sentia-hoarder](https://www.codeship.io/projects/66490d90-196e-0132-71c5-1a346f0d5e15/status)](https://www.codeship.io/projects/34383)
+Data collection server for Sentia
---
+++ test/unit/services/MapsService.test.js
@@ -0,0 +1,19 @@
+'use strict';
+var chai = require('chai'),
+  MapsService = require('../../../services/MapsService'),
+  sinon = require('sinon');
+
+chai.use(require('chai-as-promised'));
+
+describe('MapsService', function () {
+  describe('_buildQuery', function() {
+    it('should return a valid SQL query', function () {
+      var result, data;
+      data = {
+
+      }
+      // result = MapsService._buildQuery(data);
+      // result.should.equal()
+    });
+  });
+});
