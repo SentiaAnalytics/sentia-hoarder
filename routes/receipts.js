9135a655a05c7c4c35d93fad4d0b1754a99a6098
Andreas Møller <andreasmoller@Andreass-MacBook-Pro-2.local>
2014-09-22 13:16:54 +0100

    fixed issues with receipts/csv

--- routes/receipts.js
+++ routes/receipts.js
@@ -6,10 +6,10 @@ exports.csv = {
   method : 'POST',
   handler : function (req, res, next) {
     return ReceiptsService.csv(req)
-      .catch(next)
-      .done(function () {
+      .then(function () {
         return res.status(200).send('OK');
-    });
+      })
+      .catch(next);
   },
   middleware : []
 };
